f079765157caefbfcbc2189a0ca534f619be90a5
Boty Javan <inovci2@gmail.com>
2022-07-09 12:24:25 +0000

    First commit

---
+++ my_auths/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import *
+
+app_name = 'my_auths'
+
+urlpatterns = [
+    path('login/', login, name='login'),
+    path('register/', register, name='register'),
+]
---
+++ my_auths/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def login(request):
+    return render(
+        request,
+        'login/login.html'
+    )
+
+def register(request):
+    return render(
+        request,
+        'register/register.html'
+    )
